419180b2e44ecede9b6f473acfd932b0e9b915dd
nvarg <natvargas97@gmail.com>
2020-02-06 23:36:50 -0800

    camera movement

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Vector3 offset = Vector3.zero;
+
+    [SerializeField]
+    [Range(0.01f,1.0f)]
+    private float smoothingFactor = 1.0f;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, player.transform.position + offset, smoothingFactor);
+    }
+}
