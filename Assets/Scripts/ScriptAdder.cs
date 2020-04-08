bcc6301f0ee7879c41d8efc613aaced7dbc88392
gevo222 <51228298+gevo222@users.noreply.github.com>
2020-04-08 14:06:27 -0700

    Cyclone transition added. Should work with new animations too.

---
+++ Assets/Scripts/ScriptAdder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptAdder : MonoBehaviour
+{
+    public GameObject obj;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool Q = Input.GetKey(KeyCode.Q);
+        anim.SetBool("Q", Q);
+
+        if (!Q && (obj.GetComponent<CustomLocomotion>() == null))
+        {
+            obj.AddComponent<CustomLocomotion>();
+        }
+
+     
+    }
+}
