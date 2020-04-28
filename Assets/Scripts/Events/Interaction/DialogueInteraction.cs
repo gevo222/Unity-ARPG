d02d4d366a510ec5d2d1113c80ed7aa61b7a143d
nvarg <natvargas97@gmail.com>
2020-04-27 21:10:54 -0700

    NPC dialogue

---
+++ Assets/Scripts/Events/Interaction/DialogueInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using TMPro;
+
+public class DialogueInteraction : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private GameObject panel;
+
+    public void OnInteraction(GameObject actor, GameObject target)
+    {
+        if (panel.active == false) {
+            var text = target.GetComponent<Dialogue>()?.Text;
+            if (text != null)
+            {
+                panel.SetActive(true);
+                dialogueText.SetText($"<b>{target.name}</b>: {text}");
+            }
+        }
+    }
+}
+
