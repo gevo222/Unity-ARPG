1b6b620253de8d256ec3317cf14f7e610b9a6fdb
nvarg <natvargas97@gmail.com>
2020-05-12 12:27:27 -0700

    merge

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+// Enemy follows the player TODO: Make it attack the player
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+
+    Transform player;
+    NavMeshAgent agent;
+    CharacterStats playerStats;
+    CharacterStats enemyStats;
+    int elapsedTime;
+    Animator playerAnim;
+    Animator enemyAnim;
+    AudioSource fightMusic;
+    public float attackSpeed = 1f;
+    private float attackCooldown = 0f;
+
+
+
+    void Start()
+    {
+        player = Player.instance.transform;
+        agent = GetComponent<NavMeshAgent>();
+        playerStats = player.GetComponent<CharacterStats>();
+        enemyStats = GetComponent<CharacterStats>();
+        playerAnim = player.GetComponent<Animator>();
+        enemyAnim = GetComponent<Animator>();
+
+
+        if (transform.tag == "Boss")
+        {
+            fightMusic = GetComponent<AudioSource>();
+        }
+    }
+
+    void Update()
+    {
+        // check if player is within look radius
+        float distance = Vector3.Distance(player.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            // Follow the player
+            agent.SetDestination(player.position);
+            enemyAnim.SetBool("Run Forward", true);
+
+            // Enable Boss Music
+            if (transform.tag == "Boss")
+            {
+                fightMusic.enabled = true;
+            }
+
+            attackCooldown -= Time.deltaTime;
+
+            // If already in range, always face the player
+            if (distance <= agent.stoppingDistance)
+            {
+                FacePlayer();
+
+                HealOutOfCombat.playerInCombat = true;
+                enemyAnim.SetBool("Run Forward", false);
+
+                if (attackCooldown <= 0f)
+                {
+                    //Damage player's stats
+                    enemyStats.Attack(playerStats);
+
+                    //Damage enemy's stats
+                    playerStats.Attack(enemyStats);
+                    // Deal double damage with cyclone
+                    if (playerAnim.GetBool("Q"))
+                    {
+                        playerStats.Attack(enemyStats);
+                    }
+
+                    attackCooldown = 1f / attackSpeed;
+                }
+                //Play attack animation for player
+                playerAnim.SetBool("RClick", true);
+                //Play attack animation for enemy
+                enemyAnim.SetTrigger("Stab Attack");
+
+            }
+            else
+            {
+                    playerAnim.SetBool("RClick", false);
+                    HealOutOfCombat.playerInCombat = false;
+            }
+        }
+
+    }
+
+    void FacePlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+}
---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Allows EnemyController to access Damage ! not recommended
+[assembly: InternalsVisibleTo("Level")]
+
+[Serializable] public class HealthUpdateEvent : UnityEvent<Transform, int> {}
+
+public class CharacterStats : MonoBehaviour, Interactable
+{
+    public HealthUpdateEvent HealthUpdate => healthUpdate;
+
+    [SerializeField] private HealthUpdateEvent healthUpdate;
+    [SerializeField] internal Stat maxHP;
+    [SerializeField] internal Stat damage;
+    [SerializeField] private Stat armor;
+
+    private int _currentHealth;
+    public int currentHP {
+        get { return _currentHealth; }
+        private set {
+	    this.HealthUpdate?.Invoke(transform, value);
+            _currentHealth = value;
+        }
+    }
+
+    public virtual void Start()
+    {
+        if (healthUpdate == null)
+        {
+            healthUpdate = new HealthUpdateEvent();
+        }
+        currentHP = maxHP.GetStat();
+    }
+
+    public void Attack(CharacterStats target, float multiplier = 1f)
+    {
+        target.TakeDamage((int)(damage.GetStat() * multiplier));
+    }
+
+    public void TakeDamage(int rawDamage)
+    {
+        var finalDamage = Mathf.Clamp(rawDamage - armor.GetStat(), 0, 9999);
+        currentHP = currentHP - rawDamage;
+    }
+
+    public void Heal(int rawHeal)
+    {
+        currentHP = currentHP + rawHeal;
+    }
+}
