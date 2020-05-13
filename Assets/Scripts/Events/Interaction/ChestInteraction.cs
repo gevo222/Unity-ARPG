91798a1e32b7a036f7c53399328d714f3863f966
Joel <joel@none.org>
2020-05-13 09:41:02 -0700

    Add lootable chests

---
+++ Assets/Scripts/Events/Interaction/ChestInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class ChestInteraction : MonoBehaviour {
+
+    public void OnInteraction(GameObject actor, GameObject target){
+		var lootable = target.GetComponent<Lootable>();
+		if(lootable == null)
+			return;
+
+		var origin = target.transform.position;
+		Object.Destroy(target);
+
+		var itemPool = ItemObject.All.Values.ToList();
+		for(int i = 0; i < lootable.itemCount; i++){
+			var index = Random.Range(0, itemPool.Count);
+			var itemDef = itemPool[index];
+			ItemSpawner.main.SpawnNear(itemDef, origin);
+			itemPool.RemoveAt(index);
+		}
+
+	}
+}
+
